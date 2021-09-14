e2de8bca20ca16f9d74c32d12c0de4e13092c830
Sadaev <sadaevsh@gmail.com>
2021-09-14 20:58:23 +0600

    bootstrap 5

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,1 +1,64 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Beauty Salon</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+    <header class="sticky-top">
+        <div class="container text-center p-5">
+            <h2>Салон красоты</h2>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">9:00</th>
+
+                </tr>
+                <tr>
+                    <th scope="row">9:30</th>
+                </tr>
+                <tr>
+                    <th scope="row">10:00</th>
+                </tr>
+                <tr>
+                    <th scope="row">10:30</th>
+                </tr>
+                <tr>
+                    <th scope="row">11:00</th>
+                </tr>
+                <tr>
+                    <th scope="row">11:30</th>
+                </tr>
+                <tr>
+                    <th scope="row">12:00</th>
+                </tr>
+                <tr>
+                    <th scope="row">12:30</th>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+    <footer class="fixed-bottom">
+        <div class="container text-center p-5">
+            <h3>Контакты</h3>
+        </div>
+    </footer>
+</body>
+</html>
